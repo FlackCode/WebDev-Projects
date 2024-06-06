aeb9d5d8b5d0e8faf152f14b1695ee8c2a1ab597
flack <163582560+FlackCode@users.noreply.github.com>
2024-06-06 18:09:17 +0200

    Updated Mobile Version

--- todoApp/src/components/ArchivedTask.jsx
+++ todoApp/src/components/ArchivedTask.jsx
@@ -8,7 +8,7 @@ const ArchivedTask = ({ title, description, details }) => {
     }  
 
     return (
-        <div className="rounded-md px-8 py-4 border-2 cursor-pointer" onClick={handleTaskToggle}>
+        <div className="rounded-md md:px-8 xsm:px-4 md:py-4 xsm:py-2 border-2 cursor-pointer" onClick={handleTaskToggle}>
           <h1 className="text-3xl font-bold select-none">{title}</h1>
           <p className="font-semibold">{description}</p>
           {isOpened && (
--- todoApp/src/components/ToDoNav.jsx
+++ todoApp/src/components/ToDoNav.jsx
@@ -6,22 +6,22 @@ const ToDoNav = () => {
   return (
     <div className="text-text">
       <div className="xl:px-48 md:px-12 xsm:px-1 py-6 flex items-center justify-between bg-background">
-        <Link to={'/'} className='flex items-center gap-2 cursor-pointer'>
+        <Link to={'/'} className='items-center gap-2 cursor-pointer xsm:hidden md:flex'>
             <BsJournal className='md:text-xl'/>
-            <h1 className="font-bold md:text-3xl xsm:text-base">todoApp</h1>
+            <h1 className="font-bold md:text-3xl">todoApp</h1>
         </Link>
-        <div className='flex flex-row md:gap-4 xsm:gap-2 items-center'>
+        <div className='xsm:w-full md:w-fit flex flex-row xsm:justify-evenly md:justify-normal md:gap-4 xsm:gap-2 items-center xsm:px-4 md:px-0'>
           <Link to={'/archived'} className='md:text-lg xsm:text-sm font-semibold cursor-pointer transition-all duration-300 hover:scale-110'>
             Archived Tasks
           </Link>
           <Link to={'/tasks'} className='md:text-lg xsm:text-sm font-semibold cursor-pointer transition-all duration-300 hover:scale-110'>
-            View tasks
+            All tasks
           </Link>
-          <Link to={'/create'} className="md:px-6 xsm:px-4 md:py-2 xsm:py-1 bg-primary md:text-base xsm:text-sm rounded-full font-semibold transition-all duration-300 hover:scale-105 hover:bg-hover">
+          <Link to={'/create'} className="md:px-6 xsm:px-6 md:py-2 xsm:py-1 bg-primary md:text-base xsm:text-sm rounded-full font-semibold transition-all duration-300 hover:scale-105 hover:bg-hover">
             New
           </Link>
           <button className='flex items-center gap-2
-          md:px-6 xsm:px-2 md:py-3 xsm:py-1 bg-red-600 rounded-full font-semibold transition-all duration-300 hover:scale-105 hover:bg-red-700'
+          md:px-6 xsm:px-4 md:py-3 xsm:py-1 bg-red-600 rounded-full font-semibold transition-all duration-300 hover:scale-105 hover:bg-red-700'
           onClick={() => auth.signOut()}
           >
             <BsArrowRight />
